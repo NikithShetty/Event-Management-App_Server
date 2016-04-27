fc46ca72f6e7adc125af3522305f5d734635e901
Nikith Shetty <shettynikith3@gmail.com>
2016-04-27 23:26:48 +0530

    changed database url

--- controllers/DBConnect.js
+++ controllers/DBConnect.js
@@ -5,7 +5,14 @@ var mongoose = require('mongoose');
 
 
 //connect to DB
-mongoose.connect("mongodb://localhost:27017/newDB", function (err) {
+// mongoose.connect("mongodb://localhost:27017/newDB", function (err) {
+//     if(err) console.log("Error connecting to DB server : " + err);
+//     else {
+//         console.log("Connected to DB");
+//     }
+// });
+
+mongoose.connect("mongodb://nikith:newdb_mlab@ds015780.mlab.com:15780/eventmanagementdb", function (err) {
     if(err) console.log("Error connecting to DB server : " + err);
     else {
         console.log("Connected to DB");
